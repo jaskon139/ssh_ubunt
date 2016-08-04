6816f233cbdf3d1aab5ad3e7c5ed742c45805a78
jaskon139 <jaskon139@gmail.com>
2016-08-04 11:00:30 +0800

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
 name='x'
 
-for n in $(seq 1 17768) #seq是连续的意思，从1到100
+for n in $(seq 0 17768) #seq是连续的意思，从1到100
 do
   #通过awk和printf将数字变为三位并前面补0的数字，并赋值给num，
   num=$(echo $n|awk '{printf("%010d\n",$0)}') 
