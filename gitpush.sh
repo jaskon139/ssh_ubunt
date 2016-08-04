84179ffb362cd22f523094de003ad06ab4ed4ee1
jaskon139 <jaskon139@gmail.com>
2016-08-04 09:31:08 +0800

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -7,5 +7,7 @@ for n in $(seq 1 17768) #seq是连续的意思，从1到100
 do
   #通过awk和printf将数字变为三位并前面补0的数字，并赋值给num，
   num=$(echo $n|awk '{printf("%010d\n",$0)}') 
-  echo ${name}${num}
+  git add ${name}${num}
+  git commit -m "${name}${num}"
+  git push
 done
